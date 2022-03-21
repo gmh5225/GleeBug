dcb627dd401bd923146db45d07ff224038efd4ab
gmh5225 <2315157@qq.com>
2022-03-22 02:52:59 +0800

    Update to VS2022

--- TitanEngineEmulator/Global.Engine.Context.cpp
+++ TitanEngineEmulator/Global.Engine.Context.cpp
@@ -342,8 +342,9 @@ bool _SetFullContextDataEx(HANDLE hActiveThread, TITAN_ENGINE_CONTEXT_t* titcont
     DBGContext.FloatSave.ErrorOffset = titcontext->x87fpu.ErrorOffset;
     DBGContext.FloatSave.DataSelector = titcontext->x87fpu.DataSelector;
     DBGContext.FloatSave.DataOffset = titcontext->x87fpu.DataOffset;
+#ifdef _WIN64
     DBGContext.FloatSave.Cr0NpxState = titcontext->x87fpu.Cr0NpxState;
-
+#endif
     memcpy(DBGContext.FloatSave.RegisterArea, titcontext->RegisterArea, 80);
 
     // MXCSR ExtendedRegisters[24]
@@ -440,8 +441,12 @@ bool _GetFullContextDataEx(HANDLE hActiveThread, TITAN_ENGINE_CONTEXT_t* titcont
     titcontext->x87fpu.ErrorOffset = DBGContext.FloatSave.ErrorOffset;
     titcontext->x87fpu.DataSelector = DBGContext.FloatSave.DataSelector;
     titcontext->x87fpu.DataOffset = DBGContext.FloatSave.DataOffset;
+#ifdef _WIN64
     titcontext->x87fpu.Cr0NpxState = DBGContext.FloatSave.Cr0NpxState;
-
+#else
+    //[FIXME]
+    titcontext->x87fpu.Cr0NpxState = 0;
+#endif
     memcpy(titcontext->RegisterArea, DBGContext.FloatSave.RegisterArea, 80);
 
     // MXCSR ExtendedRegisters[24]
